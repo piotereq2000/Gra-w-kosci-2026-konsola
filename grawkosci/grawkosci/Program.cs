d3df9e270744f9aa12491bd6c197d44460eb96a3
piotereq2000 <149611908+piotereq2000@users.noreply.github.com>
2026-09-16 08:26:01 +0200

    Add files via upload

---
+++ grawkosci/grawkosci/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj liczbę oczek dla pierwszej kości (1-6): ");
+        int wartosc = int.Parse(Console.ReadLine());
+
+        Kosc kosc1 = new Kosc(wartosc);
+
+        Console.WriteLine();
+        Console.WriteLine("PIERWSZA KOŚĆ");
+        Console.WriteLine("Liczba utworzonych instancji: " + Kosc.liczbaInstancji);
+        Console.WriteLine("Liczba oczek: " + kosc1.liczbaOczek);
+        Console.WriteLine("Liczba oczek słownie: " + kosc1.WynikSlownie());
+        Console.WriteLine("Plik graficzny: " + kosc1.pliki[kosc1.identyfikatorPliku]);
+
+        Console.WriteLine();
+
+        Kosc kosc2 = new Kosc();
+
+        Console.WriteLine("DRUGA KOŚĆ");
+        Console.WriteLine("Liczba utworzonych instancji: " + Kosc.liczbaInstancji);
+        Console.WriteLine("Liczba oczek: " + kosc2.liczbaOczek);
+        Console.WriteLine("Liczba oczek słownie: " + kosc2.WynikSlownie());
+        Console.WriteLine("Plik graficzny: " + kosc2.pliki[kosc2.identyfikatorPliku]);
+
+        Console.WriteLine();
+        Console.WriteLine("Naciśnij dowolny klawisz, aby zakończyć...");
+        Console.ReadKey();
+    }
+}
---
+++ grawkosci/grawkosci/kosc.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+class Kosc
+{
+    public static int liczbaInstancji = 0;
+
+    public string[] pliki = {
+        "kosc0.png",
+        "kosc1.png",
+        "kosc2.png",
+        "kosc3.png",
+        "kosc4.png",
+        "kosc5.png",
+        "kosc6.png"
+    };
+
+    public int liczbaOczek;
+    public int identyfikatorPliku;
+    public bool dostepna;
+
+    private static Random losowanie = new Random();
+
+    public Kosc(int wartosc)
+    {
+        if (wartosc < 1 || wartosc > 6)
+        {
+            wartosc = 0;
+        }
+
+        liczbaOczek = wartosc;
+        identyfikatorPliku = wartosc;
+        dostepna = true;
+
+        liczbaInstancji++;
+    }
+
+    public Kosc()
+    {
+        int wartosc = losowanie.Next(1, 7);
+
+        liczbaOczek = wartosc;
+        identyfikatorPliku = wartosc;
+        dostepna = true;
+
+        liczbaInstancji++;
+    }
+
+    public void Rzut()
+    {
+        if (dostepna)
+        {
+            liczbaOczek = losowanie.Next(1, 7);
+            identyfikatorPliku = liczbaOczek;
+        }
+    }
+
+    public void Zablokuj()
+    {
+        dostepna = false;
+    }
+
+    public string WynikSlownie()
+    {
+        switch (liczbaOczek)
+        {
+            case 1:
+                return "jeden";
+            case 2:
+                return "dwa";
+            case 3:
+                return "trzy";
+            case 4:
+                return "cztery";
+            case 5:
+                return "pięć";
+            case 6:
+                return "sześć";
+            default:
+                return "zero";
+        }
+    }
+}
